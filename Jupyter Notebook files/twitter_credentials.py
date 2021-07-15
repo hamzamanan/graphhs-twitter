c0155b28fa44639f2809b92c67fddbcd035e56b6
Hamza Manan <55941197+hamzamanan@users.noreply.github.com>
2021-07-15 16:14:58 +0500

    Add files via upload

---
+++ Jupyter Notebook files/twitter_credentials.py
@@ -0,0 +1,5 @@
+consumer_key = 'hPzdDqgdoYWMMt9VgTDcijjDx'
+consumer_secret = '5ZGDuZ0T63g8dVnuAD6YU5XDYDZfAlInXeL2DxDAdqcRKpVGLK'
+access_token = '1038061436940034048-MGFXRKt75xswFbapb4REVtszm82ItE'
+access_token_secret = '2quzL6x2AP6VGzdoQst3dkD8Az8H8PYGarHYj7KjUfAP8'
+
